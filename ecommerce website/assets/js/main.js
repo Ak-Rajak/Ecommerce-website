ca38316e5f07613ddd992436827042d4229abb97
Ak-Rajak <atulkumar.coderguys@gmail.com>
2024-03-22 02:18:06 +0530

    Product Section completed

--- ecommerce website/assets/js/main.js
+++ ecommerce website/assets/js/main.js
@@ -37,3 +37,5 @@ var swiperCategories = new Swiper(".categories__container", {
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[content]');
